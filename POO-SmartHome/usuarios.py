e729b81f8ed82feea7989b52055eece5ecc62ead
Fabricio Rivarola <fabricionicolasr@gmail.com>
2025-09-14 01:50:57 -0300

    se agrega encapsulamiento

--- POO-SmartHome/usuarios.py
+++ POO-SmartHome/usuarios.py
@@ -1,15 +1,15 @@
 class Usuarios:
     def __init__(self, usuario, contraseña, dni, rol, luces):
-        self.usuario = usuario
-        self.contraseña = contraseña
-        self.dni = dni
-        self.rol = rol
-        self.luces = luces
+        self._usuario = usuario
+        self._contraseña = contraseña
+        self._dni = dni
+        self._rol = rol
+        self._luces = luces
 
-    def mostrar_datos_personales(self):
-        return f"Usuario: {self.usuario}, DNI: {self.dni}, Rol: {self.rol}"
+    def _mostrar_datos_personales(self):
+        return f"Usuario: {self._usuario}, DNI: {self._dni}, Rol: {self._rol}"
     
     def listar_dispositivos(self):
-        for luz in self.luces:
+        for luz in self._luces:
             print(luz)
         
