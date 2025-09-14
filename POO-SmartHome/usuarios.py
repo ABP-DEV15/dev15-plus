71324f89cc640ac739448fe34e2a7387064ae3af
Fabricio Rivarola <fabricionicolasr@gmail.com>
2025-09-14 01:58:53 -0300

    se agregan getters y setters en usuarios

--- POO-SmartHome/usuarios.py
+++ POO-SmartHome/usuarios.py
@@ -6,6 +6,42 @@ class Usuarios:
         self._rol = rol
         self._luces = luces
 
+    @property
+    def usuario(self):
+        return self._usuario
+
+    @usuario.setter
+    def usuario(self, nuevo_usuario):
+        self._usuario = nuevo_usuario
+
+    @property
+    def contraseña(self):
+        return self._contraseña
+    
+    @contraseña.setter
+    def contraseña(self, nueva_contraseña):
+        if len(nueva_contraseña) < 8:
+            return "La contraseña debe tener al menos 8 caracteres."
+        self._contraseña = nueva_contraseña
+
+    @property
+    def dni(self):
+        return self._dni
+    
+    @dni.setter 
+    def dni(self, nuevo_dni):
+        if len(nuevo_dni) != 8 or len(nuevo_dni) != 7:
+            return "El DNI debe tener al menos 7 caracteres y no mas de 8."
+        self._dni = nuevo_dni
+
+    @property
+    def rol(self):
+        return self._rol    
+
+    @rol.setter
+    def rol(self, nuevo_rol):
+        self._rol = nuevo_rol
+
     def _mostrar_datos_personales(self):
         return f"Usuario: {self._usuario}, DNI: {self._dni}, Rol: {self._rol}"
     
