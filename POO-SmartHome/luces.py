8e80969320bc9cc1b13ee4a90e34c3b375d2dc58
PiaPereyra <pia.pereyratauil@gmail.com>
2025-09-15 08:10:00 -0300

    metodode apagado de luces y su test

--- POO-SmartHome/luces.py
+++ POO-SmartHome/luces.py
@@ -8,31 +8,31 @@ class Luces:
             else:
                 return "apagadas"
 
+
 class Luces:
     def __init__(self):
             self._encendida
 
     def estado(self):
-            if self ._encendida:
-                return "encendidas"
-            else:
-                return "apagadas"
-    def encender(self):
-        self._encendida= True
-
-class Luces: #apagado
-    def __init__(self):
-        self._encendida = False
-
-        def estado(self):
             if self._encendida:
                 return "encendidas"
             else:
                 return "apagadas"
+    def encender(self):
+        self._encendida = True
+
+
+class Luces:
+    def __init__(self):
+        self._encendida = False
+
+        def estado(self):
+            if self._encendida:True
+            return "encendidas"
+            return "apagadas"
 
     def encender(self):
         self._encendida = True
 
     def apagar(self):
         self._encendida = False
-
