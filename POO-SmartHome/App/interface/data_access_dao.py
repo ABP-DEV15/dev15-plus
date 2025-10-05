b3c6628f21a4e93291b1196f117698804d9cc8ff
Fabricio Rivarola <fabricionicolasr@gmail.com>
2025-10-05 11:30:40 -0300

    se agrega carpeta interface y archivo interface

---
+++ POO-SmartHome/App/interface/data_access_dao.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class DataAccessDAO(ABC):
+    @abstractmethod
+    def get(self, id):
+        pass
+
+    @abstractmethod
+    def get_all_data(self):
+        pass
+
+    @abstractmethod
+    def create(self, object):
+        pass
+
+    @abstractmethod
+    def update(self, object):
+        pass
+
+    @abstractmethod
+    def delete(self, object):
+        pass
+    
