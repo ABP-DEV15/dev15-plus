8c3d91f62579fec9b862bc5a3515c505074ff5fe
Fabricio Rivarola <fabricionicolasr@gmail.com>
2025-10-12 12:13:56 -0300

    se completa usuario regular

--- POO-SmartHome/App/dominio/usuario_regular.py
+++ POO-SmartHome/App/dominio/usuario_regular.py
@@ -3,3 +3,6 @@ from clases_base.usuarios import Usuario
 class UsuarioRegular(Usuario):
     def __init__(self, usuario, contraseña, dni):
         super().__init__(usuario, contraseña, dni, rol='regular')
+
+    def consultar_datos(self):
+        return f"Usuario: {self.usuario}, DNI: {self.dni}, Rol: {self.rol}"
