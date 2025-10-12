be156a0d346e8cbf85afb7d2bed7d092818eb131
Fabricio Rivarola <fabricionicolasr@gmail.com>
2025-10-12 12:17:29 -0300

    se cambia usuario regular a estandar

---
+++ POO-SmartHome/App/dominio/usuario_administrador.py
@@ -0,0 +1,5 @@
+from clases_base.usuarios import Usuario
+
+class UsuarioAdministrador(Usuario):
+    def __init__(self, usuario, contraseña, dni):
+        super().__init__(usuario, contraseña, dni, rol='administrador')
--- POO-SmartHome/App/dominio/usuario_estandar.py
+++ POO-SmartHome/App/dominio/usuario_estandar.py
@@ -1,8 +1,8 @@
 from clases_base.usuarios import Usuario
 
-class UsuarioRegular(Usuario):
+class UsuarioEstandar(Usuario):
     def __init__(self, usuario, contraseña, dni):
-        super().__init__(usuario, contraseña, dni, rol='regular')
+        super().__init__(usuario, contraseña, dni, rol='estandar')
 
     def consultar_datos(self):
         return f"Usuario: {self.usuario}, DNI: {self.dni}, Rol: {self.rol}"
