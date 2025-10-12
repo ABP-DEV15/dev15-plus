0c32c506d5291b387ff2877442bc1df7e1f57746
pgatica <gatica489@gmail.com>
2025-10-12 00:02:30 -0300

    Eliminamos luces_controladores.py

---
+++ POO-SmartHome/App/dao/luces_dao.py
@@ -0,0 +1,50 @@
+from conn.dbconn import DBConn
+from interface.data_access_dao import DataAccessDAO
+from dominio.luces import Luces
+
+class LucesDAO(DataAccessDAO):
+    def __init__(self, dbconn: DBConn):
+        self.dbconn = dbconn.conectar_a_mysql()
+        self.database = dbconn.obtener_base_de_datos()
+
+    def get(self, id):
+        with self.dbconn as conn:
+            cursor = conn.cursor()
+            query = f"SELECT id_luz, nombre, intensidad, id_vivienda FROM {self.database}.luces WHERE id_luz = %s"
+            cursor.execute(query, (id,))
+            row = cursor.fetchone()
+            if row:
+                return Luces(nombre=row[1], intensidad=row[2])
+            return None
+
+    def get_all_data(self):
+        with self.dbconn as conn:
+            cursor = conn.cursor()
+            query = f"SELECT id_luz, nombre, intensidad, id_vivienda FROM {self.database}.luces"
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            luces = [Luces(nombre=row[1], intensidad=row[2]) for row in rows]
+            return luces
+
+    def create(self, luz: Luces):
+        with self.dbconn as conn:
+            cursor = conn.cursor()
+            query = f"INSERT INTO {self.database}.luces (nombre, intensidad) VALUES (%s, %s)"
+            values = (luz.nombre, luz.intensidad)
+            cursor.execute(query, values)
+            conn.commit()
+
+    def update(self, luz: Luces):
+        with self.dbconn as conn:
+            cursor = conn.cursor()
+            query = f"UPDATE {self.database}.luces SET nombre = %s, intensidad = %s WHERE nombre = %s"
+            values = (luz.nombre, luz.intensidad, luz.nombre)
+            cursor.execute(query, values)
+            conn.commit()
+
+    def delete(self, nombre):
+        with self.dbconn as conn:
+            cursor = conn.cursor()
+            query = f"DELETE FROM {self.database}.luces WHERE nombre = %s"
+            cursor.execute(query, (nombre,))
+            conn.commit()
